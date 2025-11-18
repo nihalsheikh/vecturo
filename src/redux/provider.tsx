c6132cca3257e2ba8145445df97fcd85b6958968
nihal <nihalsheikh585@gmail.com>
2025-11-18 15:58:32 +0530

    ADDED: lazy initializer with /redux/provider.ts

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode, useRef } from "react";
-import { makeStore, RootState } from "./store";
+import { makeStore, RootState, AppStore } from "./store";
 import { Provider } from "react-redux";
 
 const ReduxProvider = ({
@@ -11,7 +11,10 @@ const ReduxProvider = ({
 	children: ReactNode;
 	preloadedState: Partial<RootState>;
 }) => {
-	const storeRef = useRef(makeStore(preloadedState));
+	const storeRef = useRef<AppStore | null>(null);
+	if (!storeRef.current) {
+		storeRef.current = makeStore(preloadedState);
+	}
 
 	return <Provider store={storeRef.current}>{children}</Provider>;
 };
